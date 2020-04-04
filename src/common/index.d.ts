e618c211c582f4ff2840a77e06f402230bf3bbd0
suminlee <suminpixel@gmail.com>
2020-04-04 20:00:33 +0900

    todo add api

--- src/common/index.d.ts
+++ src/common/index.d.ts
@@ -1,5 +1,15 @@
 interface IResponseBody {
-    result: boolean;
-    message?: string;
-    data?: any;
-  }
+  result: boolean;
+  message?: string;
+  data?: any;
+}
+
+interface ITodo {
+
+  id: string;
+  deletedAt: -1 | string;
+  createdAt:  string | number;
+  isCompleted: boolean;
+  task: string;
+
+}
